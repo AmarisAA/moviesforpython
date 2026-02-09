d0c27b08eeb19afa2b36e285b9201f3143e2f729
AmarisAA <amarislvrz@gmail.com>
2026-02-08 19:55:51 -0600

    Add files via upload

---
+++ movies/api/admin.py
@@ -0,0 +1,12 @@
+#movies/api/admin.py
+from django.contrib import admin
+from .models import Movie
+
+class MovieList(admin.ModelAdmin):
+    list_display = ('name', 'year', 'description', 'rating', 'movie_image')
+    list_filter = ('name', 'year', 'rating', 'movie_image')
+    search_fields = ('name', 'description')
+    ordering = ['year']
+
+admin.site.register(Movie, MovieList)
+
